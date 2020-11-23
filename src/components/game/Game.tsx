5b06e596f44059e7e92e28d59a17036cbe84c1d0
Máté Varga <mate.varga@talentuno.com>
2020-11-23 15:22:38 +0100

    FIX: Canvas

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as PIXI from 'pixi.js'
-import { Layout, Row, Col, Image, Space, Typography } from 'antd';
+import { Layout, Row, Col, Image, Space, Typography, Button } from 'antd';
 
 import { setBackground } from './background';
 import { addXWing, handleShooting } from './x-wing';
@@ -9,28 +9,33 @@ import { handleGame } from './handleGame';
 
 const { Title, Text } = Typography;
 
-const app = new PIXI.Application({
-    width: 800, height: 600, backgroundColor: 0x030F12, resolution: window.devicePixelRatio || 1
-});
-
-document.body.appendChild(app.view);
-
-setBackground(app);
-const xWing = addXWing(app);
-const bullets = handleShooting(app);
-const tieFighters = addTieFighters(app);
-handleGame(app, xWing, bullets, tieFighters);
-
-const playground = document.getElementById('pxrender');
-if (playground) {
-    playground.appendChild(app.view);
-    app.start();
-}
-
 const Game: React.FC = () => {
+    const app = new PIXI.Application({
+        width: 800,
+        height: 600,
+        backgroundColor: 0x030F12,
+        resolution: window.devicePixelRatio || 1,
+        forceCanvas: false,
+    });
+
+    useEffect(() => {
+        const playground = document.getElementById('playground');
+        if (playground) {
+            playground.appendChild(app.view);
+            app.start();
+        }
+    }, [app]);
+
+    setBackground(app);
+    const xWing = addXWing(app);
+    const bullets = handleShooting(app);
+    const tieFighters = addTieFighters(app);
+    handleGame(app, xWing, bullets, tieFighters);
+
     return (
         <Layout style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', bottom: 0 }}>
-            <div id="pxrender"></div>
+            <div id="playground"></div>
+
             <Title>Controls</Title>
             <Space size={200}>
                 <Text>Shoot</Text>
@@ -63,6 +68,11 @@ const Game: React.FC = () => {
                         />
                     </Row>
                 </Col>
+            </Row>
+            <Row style={{ paddingTop: 50 }}>
+                <Button type="primary" shape="round" size="large" href="/">
+                    BACK TO MENU
+			    </Button>
             </Row>
         </Layout>
     )
