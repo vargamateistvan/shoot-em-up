051e3a55e57879e4305a62c1e6b39f2400f82394
Máté Varga <mate.varga@talentuno.com>
2020-11-23 14:46:38 +0100

    ADD: Controls explanation

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import * as PIXI from 'pixi.js'
-import { Layout } from 'antd';
+import { Layout, Row, Col, Image, Space, Typography } from 'antd';
 
 import { setBackground } from './background';
 import { addXWing, handleShooting } from './x-wing';
 import { addTieFighters } from './tie-figter';
 import { handleGame } from './handleGame';
+
+const { Title, Text } = Typography;
 
 const app = new PIXI.Application({
     width: 800, height: 600, backgroundColor: 0x030F12, resolution: window.devicePixelRatio || 1
@@ -27,8 +29,41 @@ if (playground) {
 
 const Game: React.FC = () => {
     return (
-        <Layout>
+        <Layout style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', bottom: 0 }}>
             <div id="pxrender"></div>
+            <Title>Controls</Title>
+            <Space size={200}>
+                <Text>Shoot</Text>
+                <Text>Movement</Text>
+            </Space>
+            <Row align="bottom" justify="space-around">
+                <Col>
+                    <Image
+                        width={300}
+                        src="https://www.wpclipart.com/dl.php?img=/computer/keyboard_keys/large_keys/computer_key_Space_bar_T.png"
+                    />
+                </Col>
+                <Col>
+                    <Row justify="center">
+                        <Image
+                            width={50}
+                            src="https://www.wpclipart.com/dl.php?img=/computer/keyboard_keys/arrow_keys/computer_key_Arrow_Up_T.png"
+                        />
+                    </Row>
+                    <Row>
+                        <Image
+                            width={50}
+                            src="https://www.wpclipart.com/dl.php?img=/computer/keyboard_keys/arrow_keys/computer_key_Arrow_Left_T.png"
+                        /><Image
+                            width={50}
+                            src="https://www.wpclipart.com/dl.php?img=/computer/keyboard_keys/arrow_keys/computer_key_Arrow_Down_T.png"
+                        /><Image
+                            width={50}
+                            src="https://www.wpclipart.com/dl.php?img=/computer/keyboard_keys/arrow_keys/computer_key_Arrow_Right_T.png"
+                        />
+                    </Row>
+                </Col>
+            </Row>
         </Layout>
     )
 }
